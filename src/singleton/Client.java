9f3cdba2262b9906013b1906838b674e3d84eccd
Fly-Gao <2658063704@qq.com>
2018-01-20 21:14:57 +0800

    Singleton

--- src/singleton/Client.java
+++ src/singleton/Client.java
@@ -1,18 +1,35 @@
 package singleton;
 
+import java.io.*;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 public class Client {
-    public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException, InstantiationException {
+    public static void main(String[] args) throws Exception {
         SingletonOne singletonOne = SingletonOne.getInstance();
         SingletonOne singletonOne2 = SingletonOne.getInstance();
         System.out.println(singletonOne == singletonOne2);
 
 
+        //通过反射机制破解单例模式
+        /*Class<?> clazz = Class.forName("singleton.SingletonOne");
+        Constructor<?> constructor = clazz.getDeclaredConstructor();
+        constructor.setAccessible(true);
+        SingletonOne instance = (SingletonOne) constructor.newInstance();
+        System.out.println(singletonOne == instance);*/
 
-        Class<?> clazz = Class.forName("singleton.SingletonOne");
+        //通过序列化和反序列化破解单例模式
+        FileOutputStream fos = new FileOutputStream("D:\\obj.txt");
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+        oos.writeObject(singletonOne);
 
-        SingletonOne instance = (SingletonOne) clazz.newInstance();
-        System.out.println(singletonOne == instance);
-
-
+        FileInputStream fis = new FileInputStream("D:\\obj.txt");
+        ObjectInputStream ois = new ObjectInputStream(fis);
+        SingletonOne o = (SingletonOne) ois.readObject();
+        ois.close();
+        fis.close();
+        oos.close();
+        fos.close();
+        System.out.println(o == singletonOne);
     }
 }
--- src/singleton/SingletonOne.java
+++ src/singleton/SingletonOne.java
@@ -1,10 +1,13 @@
 package singleton;
 
+import java.io.ObjectStreamException;
+import java.io.Serializable;
+
 /**
  * 通过单例模式可以保证系统中，应用该模式的类只有一个实例，通常用于创建数据库连接，使用BIO对文件进行操作时，
  * 必须保证一次只有一个实例对该指定的对象进行操作。
  */
-public class SingletonOne {
+public class SingletonOne implements Serializable {
     //使用饿汗式创建单利模式，在Java初始化类的时候会优先初始化静态变量，并保持全局只有一个该对象，但是不能延时加载
     private static SingletonOne instance = new SingletonOne();
 
@@ -15,5 +18,13 @@ public class SingletonOne {
 
     //私有化构造器，禁止外部通过构造器创建实例
     private SingletonOne() {
+        if (instance != null) {
+            throw new RuntimeException();
+        }
+    }
+
+    //通过抛出ObjectStreamException，防止反序列化再次创建对象
+    private Object readResolve() throws ObjectStreamException {
+        return instance;
     }
 }
