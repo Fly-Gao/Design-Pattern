d6ff52d91a106633c80abf64f6de3414876c2802
Fly-Gao <2658063704@qq.com>
2018-01-20 20:45:20 +0800

    Singleton

---
+++ src/singleton/Client.java
@@ -0,0 +1,14 @@
+package singleton;
+
+public class Client {
+    public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException, InstantiationException {
+        SingletonOne singletonOne = SingletonOne.getInstance();
+        SingletonOne singletonOne2 = SingletonOne.getInstance();
+        System.out.println(singletonOne == singletonOne2);
+
+        Class<?> clazz = Class.forName("singleton.SingletonOne");
+
+        SingletonOne instance = (SingletonOne) clazz.newInstance();
+        System.out.println(singletonOne == instance);
+    }
+}
